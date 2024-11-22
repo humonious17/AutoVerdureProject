2604745edf692f91245b55087c976457e2b5fd13
SARIKA AGRAWAL <114508586+Sarika-Agrawal@users.noreply.github.com>
2024-11-22 16:11:27 +0530

    Update product icons

--- app/ui/Store/ProductCard.jsx
+++ app/ui/Store/ProductCard.jsx
@@ -67,9 +67,8 @@ const ProductCard = ({ product, viewType = "grid" }) => {
           {/* Product Icons */}
           {(plants || flowers || planters || accessory) && (
             <div
-              className={`w-fit flex flex-wrap gap-[6px] sm:gap-[12px] 
-      ${isListView ? "flex-row sm:flex-col" : "flex-row sm:flex-col"}
-      absolute top-[10px] right-[10px] sm:top-[21.18px] sm:right-[29px]`}
+            className={`w-fit flex flex-col gap-[6px] sm:gap-[12px] 
+              absolute top-[10px] right-[10px] sm:top-[21.18px] sm:right-[29px]`}
             >
               {product.petFriendly && (
                 <div className="w-[28px] h-[28px] sm:w-[52px] sm:h-[52px] p-[4px] sm:p-[10px] rounded-xl sm:rounded-2xl bg-[#FFFFFF]">

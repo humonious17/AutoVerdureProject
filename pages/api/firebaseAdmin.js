7911ee7fa827f0428f9167fb01d4b7509c07dc2a
humonious17 <abhishek.chandel2117@gmail.com>
2024-12-22 12:37:09 +0530

    Refactor Firebase initialization to use service account credentials for improved security

--- pages/api/firebaseAdmin.js
+++ pages/api/firebaseAdmin.js
@@ -1,17 +1,12 @@
 import admin from "firebase-admin";
-import { initializeApp } from "firebase/app";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDF8C6kkHyW7HAXJt_aouMceH-O-IQlemQ",
-  authDomain: "elegant-works-429712-a7.firebaseapp.com",
-  projectId: "elegant-works-429712-a7",
-  storageBucket: "elegant-works-429712-a7.appspot.com",
-  messagingSenderId: "39593396169",
-  appId: "1:39593396169:web:db2619ae24cc9ccfdc0a18",
-};
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEYS);
 
 if (!admin.apps.length) {
-  admin.initializeApp(firebaseConfig);
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: "elegant-works-429712-a7.appspot.com",
+  });
 }
 
 const auth = admin.auth();
